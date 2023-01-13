d8587e176224ffed400e29458d72a86e50b5c13e
Ibragimovic <m.ibragim88@gmail.com>
2023-01-13 21:25:24 +0600

    solved the sum-of-even-numbers

---
+++ sum-of-even-numbers/src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
@@ -0,0 +1,22 @@
+package com.epam.rd.autotasks.arrays;
+
+public class SumOfEvenNumbers {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 3, 2, 8, 15, 199};
+        System.out.println(sum(array));
+    }
+
+    public static int sum(int[] array){
+        int sum = 0;
+        if (array == null) {
+            return sum;
+        }
+        for (int elem : array) {
+            if (elem % 2 == 0) {
+                sum += elem;
+            }
+        }
+        return sum;
+    }
+}
